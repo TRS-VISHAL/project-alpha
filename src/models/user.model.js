af063957e396fbd2552e9de1a4d9f1facca96ae1
TRS-VISHAL <vishalsambare2004@gmail.com>
2024-08-05 22:13:30 +0530

    models, jwt,  bcrypt

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avtar: {
+      type: String, //CLOUDANARY URL
+      default: "default.jpg",
+      required: true,
+    },
+    coverImage: {
+      type: String, //CLOUDANARY URL
+      default: "default-cover.jpg",
+    },
+    watchHistroy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// below code is for encrypting the password with bycrypt
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// below code is for validating the password with bycrypt
+
+userSchema.methods.isPasswordcorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// below code is for generating JWT (access token)
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// below code is for jwt (refresh token ) both are same 
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,18 @@
 const asyncHandler = (reqHandler) => {
-     (req , res , next) => {
-          Promise.resolve(reqHandler(req , res , next)).catch((err)=>{
-               next(err)
-          })
-     }
-}
-export { asyncHandler }
+  (req, res, next) => {
+    Promise.resolve(reqHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+export { asyncHandler };
+
+// const asyncHandler1 = (reqHandler1) => async (req, res, next) => {
+//   try {
+//     await reqHandler1(req, res, next);
+//   } catch (err) {
+//     next(err);
+//     res.status(500).send;
+//   }
+// };
+``
